626ec32043f20ac3bc0b33562d99d0c5eaea7842
mehmet burak sayıcı <mehmetburaksayici@Mehmets-MacBook-Air.local>
2024-02-24 13:43:25 +0300

    feat(chainlit): chainlit frontend added

---
+++ frontend/chainlit_app.py
@@ -0,0 +1,80 @@
+import io
+import os
+import sys
+from typing import Optional
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+import chainlit as cl
+
+from core.engine.driver import ModelDriver
+
+model_driver = ModelDriver()
+
+
+@cl.password_auth_callback
+def auth_callback(username: str, password: str):
+    # Fetch the user matching username from your database
+    # and compare the hashed password with the value stored in the database
+    if (username, password) == ("burak", "sean"):  # temporary solution
+        return cl.User(
+            identifier="admin", metadata={"role": "admin", "provider": "credentials"}
+        )
+    else:
+        return None
+
+
+@cl.on_chat_start
+async def on_chat_start():
+    model_driver.set_pdf_loader("PyPDFLoader")
+    model_driver.load_model("mistral-api")
+
+    # Sending an image with the local file path
+    await cl.Message(
+        content="Hello there, Welcome to AskAnyQuery related to Data!"
+    ).send()
+
+    files = None
+
+    # Waits for user to upload csv data
+    while files is None:
+        files = await cl.AskFileMessage(
+            content="Please upload a pdf file to begin!",
+            accept=["application/pdf"],
+            max_size_mb=100,
+        ).send()
+
+    # load the csv data and store in user_session
+    file = files[0]
+    # creating user session to store data
+
+    cl.user_session.set("data", file.path)
+    model_driver.load_document(file.path)
+    cl.user_session.set("chain", model_driver.get_chain())
+
+    # Send response back to user
+    await cl.Message(
+        content=f"`{file.name}` uploaded! Now you ask me anything related to your data"
+    ).send()
+
+
+@cl.on_message
+async def main(message: str):
+
+    """cb = cl.AsyncLangchainCallbackHandler(
+    stream_final_answer=True, answer_prefix_tokens=["FINAL","ANSWER"]
+    )
+    """
+    msg = cl.Message(content="")
+
+    await msg.send()
+
+    # Send a response back to the user
+    # async for chunk in model_driver.chat_chain_injected(message,qa_chain=cl.user_session.get('chain')):
+    #    await msg.stream_token(chunk)
+
+    chain = cl.user_session.get("chain")
+    async for chunk in chain.astream({"query": message.content}):
+        await msg.stream_token(str(chunk["result"]))
+
+    await msg.update()
---
+++ frontend/streamlit_app.py
@@ -0,0 +1,64 @@
+import requests
+import streamlit as st
+from PIL import Image
+
+
+def send_pdf_to_fastapi(pdf_file):
+    url = "http://localhost:8080/admin/upload_pdf/"
+    files = {"pdf_file": pdf_file}
+    response = requests.post(url, files=files)
+    return response.json()
+
+
+# Function to send query to chatbot endpoint
+def send_query_to_chatbot(pdf_link, query):
+    url = "http://localhost:8080/admin/chat/"
+    params = {"pdf_link": pdf_link, "query": query}
+    return requests.get(url, params=params, stream=True)
+
+
+# Define function to interact with backend API
+def make_api_request():
+    # Code to interact with your backend API goes here
+    # Replace this with actual API call
+    return "API request made successfully"
+
+
+# Set up layout
+st.sidebar.title("Conversations")
+# Display conversation list
+st.sidebar.text_area("Conversation List", [], height=400, key="conversation_list")
+
+st.title("Chatbot App")
+
+# Upload PDF file
+pdf_file = st.file_uploader("Upload PDF", type=["pdf"])
+
+
+# UI for sending PDF to FastAPI
+pdf_button = st.button("Send PDF to FastAPI", key="pdfsend")
+if pdf_button and pdf_file:
+
+    response = send_pdf_to_fastapi(pdf_file)
+    uploaded_filename = response["filename"]
+
+    st.session_state["uploaded_filename"] = uploaded_filename
+
+    st.write("Response:", response)
+elif pdf_button and not pdf_file:
+    st.warning("Please upload a PDF file before sending.")
+
+
+# Chatbot section
+st.write("## Chatbot")
+input_text = st.text_input("Enter your message:", key="chatbottext")
+if st.button("Send"):
+    if input_text:
+        response = send_query_to_chatbot(
+            st.session_state["uploaded_filename"], input_text
+        )
+        # conversation_list += "\nYou: " + input_text + "\nBot: " + response + "\n"
+        st.markdown(response.content.decode("utf-8"))
+
+    else:
+        st.warning("Please enter a message.")
